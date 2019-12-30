e51a708aa85da6f49eec26f1cd15c1e5cd4577fd
YUKI Kaoru <yuki@yagni.jp>
2019-12-31 04:17:32 +0900

    wrong assert value

--- tests/test_query.py
+++ tests/test_query.py
@@ -4,5 +4,5 @@ from csrestructuredquery.query import Query
 
 
 def test_文字列型と日時型は引用符で括られる():
-    q = Query(("foo", "hoge"), ("bar", 123), ("baz", datetime(2013, 1, 23)))
-    assert q.build() == "(and foo:'hoge' bar:123 baz:'1923-09-01T11:58:32.123456')"
+    q = Query(("foo", "hoge"), ("bar", 123), ("baz", datetime(2013, 1, 23, 12, 34, 56)))
+    assert q.build() == "(and foo:'hoge' bar:123 baz:'2013-01-23T12:34:56')"
